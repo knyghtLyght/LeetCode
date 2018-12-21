d47eae8ddb11158585288209bf4c5397c98fe854
knyghtlyght <kevnaf@gmail.com>
2018-12-20 21:16:49 -0800

    zigzag first pass

---
+++ ZigZagString/ZigZagString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ZigZagString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ZigZag("PAYPALISHIRING", 3));
+            Console.ReadLine();
+        }
+
+        static string ZigZag(string s, int numRows)
+        {
+            if (numRows <= 1) return s;
+
+            string results = "";
+
+            for (int rowIndex = 0; rowIndex < numRows; rowIndex++)
+            {
+                for (int stringIndex = 0; stringIndex < s.Length; stringIndex++)
+                {
+                    results += s[stringIndex];
+
+                }
+            }
+
+            return results;
+        }
+    }
+}
